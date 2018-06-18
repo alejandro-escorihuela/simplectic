f101eb498a1792e4ec4333db7fbea57fc9813699
cosmogat <cosmogatespacial@gmail.com>
2018-06-18 12:53:06 +0200

    new splitting 4th order method: 6 steps

---
+++ src/13_s46s.c
@@ -0,0 +1,78 @@
+/* 18-06-2018 */
+/* alex */
+/* 13_s46s.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "solar.h"
+
+int main (int num_arg, char * vec_arg[]){
+  int i, j, k, it, planetes, N, pop, pit, Neval = 0;
+  char noms[MAX_PLA][MAX_CAD], f_ini[20];
+  real masses[MAX_PLA], q[MAX_PLA][COMP], p[MAX_PLA][COMP];
+  real H0, H, DH, Hemax = 0.0, gT, gV;
+  real h;
+  int s = 6;
+  real a[2 * s], ah[2 * s];
+  double t0, t = 0.0;
+  FILE * fit_pl[MAX_PLA + 1];
+
+  carregar_configuracio(num_arg, vec_arg, &h, &N, &pop, &pit, f_ini);
+  planetes = carregar_planetes(f_ini, masses, noms, q, p);
+  H0 = energia(masses, q, p, planetes);
+  obrir_fitxers(fit_pl, noms, f_ini, vec_arg[0], planetes);
+  a[0] = a[11] = 0.0792036964311960L;
+  a[1] = a[10] = 0.1303114101821661L;
+  a[2] = a[9] = 0.2228614958676080L;
+  a[3] = a[8] = -0.3667132690474261L;
+  a[4] = a[7] = 0.3246481886897060L;
+  a[5] = a[6] = 0.5 - (a[0] + a[1] + a[2] + a[3] + a[4]);
+  for (i = 0; i < (2 * s); i++) {
+    ah[i] = a[i] * h;
+  }
+  
+  /* Mètode d'escissió */  
+  for (it = 0; it < N; it++) {
+    t0 = temps();
+    /* Bucle per a k */
+    for (k = 0; k < (2 * s); k += 2) {
+      for (i = 1; i < planetes; i++) {
+	for (j = 0; j < COMP; j++) {
+	  gV = gradV(masses, q, i, j, planetes);
+	  p[i][j] -= ah[k] * gV;
+	}
+      } 
+      for (i = 1; i < planetes; i++) {
+	for (j = 0; j < COMP; j++) {
+	  gT = (p[i][j] / masses[i]);
+	  q[i][j] += ah[k] * gT;
+	}
+      }   
+      
+      for (i = 1; i < planetes; i++) {
+	for (j = 0; j < COMP; j++) {
+	  gT = (p[i][j] / masses[i]);
+	  q[i][j] += ah[k + 1] * gT;
+	}
+      }
+      for (i = 1; i < planetes; i++) {
+	for (j = 0; j < COMP; j++) {
+	  gV = gradV(masses, q, i, j, planetes);
+	  p[i][j] -= ah[k + 1] * gV;
+	}
+      }  
+    }
+
+    Neval += (s * (planetes - 1));
+    t += temps() - t0;
+    H = energia(masses, q, p, planetes);
+    DH = fabs(H - H0);
+    if (DH > Hemax)
+      Hemax = DH;
+    if ((it % pit) == 0)
+      escriure_fitxers(fit_pl, pop, ((real) it) * h, q, p, H0, H, planetes);
+  }
+  tancar_fitxers(fit_pl, planetes);
+  print_info(h, t, Neval, Hemax / H0);
+  return 0;
+}
