b537d4b0502fda58d9b3f86378abeedfc75987cc
alescori <cosmogatespacial@gmail.com>
2018-11-15 12:48:41 +0100

    add methods 4 steps for 4th order

--- plt/ordre4s.sh
+++ plt/ordre4s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 26-04-2018
+# 15-11-2018
 # alex
-# ordre.sh
+# ordre4s.sh
 
 PAS=()
 MET=()
@@ -11,10 +11,15 @@ PAS[2]=100
 PAS[3]=50
 PAS[4]=25
 PAS[5]=10
-MET[0]="sb_6_4"
-MET[1]="ss_5_4"
-MET[2]="sa_5_4_3"
-MET[3]="sa_4_4_2"
+MET[0]="sa_4_4_3"
+MET[1]="sa_4_4_8"
+MET[2]="sa_4_4_2"
+MET[3]="sa_4_4_1"
+MET[4]="sa_4_4_4"
+MET[5]="sa_4_4_5"
+MET[6]="sa_4_4_6"
+MET[7]="sa_4_4_7"
+MET[8]="sa_4_4_9"
 
 cd ..
 mkdir .tmp_dat
@@ -32,7 +37,7 @@ echo "reset" >> plot.plt
 echo "set terminal pdf enhanced font 'Verdana, 10'" >> plot.plt
 echo "set key font ',8'" >> plot.plt
 echo "set xtics font 'Verdana,8'" >> plot.plt
-echo "set output \"../graf/ordre.pdf\"" >> plot.plt
+echo "set output \"../graf/ordre4s.pdf\"" >> plot.plt
 echo "set key out vert" >> plot.plt
 echo "set key right" >> plot.plt
 echo "set title \"Efficiency\"" >> plot.plt
@@ -44,7 +49,7 @@ for i in ${MET[@]} ; do
     k=$(echo $i | cut -d'_' -f 1 | tr [[:lower:]] [[:upper:]])
     l=$(echo $i | cut -d'_' -f 2)
     m=$(echo $i | cut -d'_' -f 3)
-    #n=$(echo $i | cut -d'_' -f 4)
+    n=$(echo $i | cut -d'_' -f 4)
     l="_$l^{[$m]}"
     echo -n "\"$i.dat\" u (log10(\$3)):(log10(\$4)) t \"$k$l $n\" w lp lw 2 ps 0.5 pt 2, " >> plot.plt
     let "IT++"
